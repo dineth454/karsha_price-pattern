439633d9a3594903028898133dd56d9465e5c557
Anushka <anu.udb@gmail.com>
2016-11-24 15:54:23 +0530

    Assign values to Y axis

--- Price_Pattern/src/com/jwt/hibernate/controller/aggregateController.java
+++ Price_Pattern/src/com/jwt/hibernate/controller/aggregateController.java
@@ -19,24 +19,21 @@ public class aggregateController {
 	@Path("")
 	@GET
 	@Produces("application/json")
-	public Response generateJson(){
+	public Response generateJson() {
 		aggregateDAO data = new aggregateDAO();
-		List<Aggregate>  list =  data.getDetails();
-		//company.genJson(list, request, response);
+		List<Aggregate> list = data.getDetails();
+		// company.genJson(list, request, response);
 		JSONArray jsonArray = new JSONArray();
-		
-		
-		for(Aggregate Aggregate : list)
-        {
-			
+
+		for (Aggregate Aggregate : list) {
 			JSONObject jsonObject = new JSONObject();
-			jsonObject.put("date",Aggregate.getDATE());
-			jsonObject.put("peak_gain",Aggregate.getGAIN());
-			jsonObject.put("peak_loss",Aggregate.getLOSS());
+			jsonObject.put("date", Aggregate.getDATE());
+			jsonObject.put("peak_gain", Aggregate.getGAIN());
+			jsonObject.put("peak_loss", Aggregate.getLOSS());
 			jsonArray.put(jsonObject);
-			//jsonObject.put(companydetails.getPRC());
-        }	
-		
-		return Response.status(200).entity(jsonArray.toString()).build();	
+			// jsonObject.put(companydetails.getPRC());
+		}
+
+		return Response.status(200).entity(jsonArray.toString()).build();
 	}
 }
